99347c7e97fac01203cb1963543dbb7b0b70eb03
git status <luyuanxin1995@gmail.com>
2018-04-06 21:17:00 -0400

    people list and type space

--- frontend/containers/ChatContainer.js
+++ frontend/containers/ChatContainer.js
@@ -10,8 +10,29 @@ const ChatContainer = ( {} ) => {
           <div className="chat-box-wrapper">
             <div className="chat-box-container">
               <div className="chat-box-people-list-container">
+                <div className="chat-box-people-list-people">
+                  <img className="chat-box-people-list-profile-pic"
+                    src="http://res.cloudinary.com/dclmhv0zu/image/upload/v1523040961/555cd827049321.5635f4f5b110f.png"/>
+                  <div className="chat-box-people-list-name">
+                    Vincent Liu
+                  </div>
+                </div>
               </div>
               <div className="chat-box-chat-container">
+                <div className="chat-box-chat-container-chat-space">
+
+                </div>
+                <div className="chat-box-chat-container-type-space">
+                  <div className="chat-box-chat-container-type-space-wrapper">
+                    <div className="chat-box-chat-container-type-space-text-area-wrapper">
+                      <textarea
+                        placeholder="Type a message..."
+                        rows="1"
+                        className="chat-box-chat-container-type-space-text-area">
+                      </textarea>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
